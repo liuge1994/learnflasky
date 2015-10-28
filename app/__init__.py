a24b0bb79bd54d1cbd75f82f1bb7c0d82a958c8c
liuge1994 <610562301@qq.com>
2015-10-28 21:08:33 +0800

    add error

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ moment = Moment()
 db = SQLAlchemy()
 
 def create_app(config_name):
+    
     app = Flask(__name__)
     app.config.from_object(config_name])
     config[config_name].init_app(app)
@@ -18,5 +19,9 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
